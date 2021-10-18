803a24fa85b5d5652aa36589f4b2103c414d6981
Damian Zaremba <damian@damianzaremba.co.uk>
2021-10-18 16:43:58 +0200

    var_dump -> var_export, so the log message contains the output

--- src/Wikipedia/Http.php
+++ src/Wikipedia/Http.php
@@ -147,14 +147,14 @@ class Http
             if (array_key_exists('error', $response)) {
                 $caller = (new \Exception())->getTrace()[1]['function'];
                 $this->logger->addError($caller . ' API Error: ' .
-                                        var_dump($response['error']));
+                                        var_export($response['error'], true));
             }
 
             // Handle warnings
             if (array_key_exists('warnings', $response)) {
                 $caller = (new \Exception())->getTrace()[1]['function'];
                 $this->logger->addWarning($caller . ' API Warnings: ' .
-                                          var_dump($response['warnings']));
+                                          var_export($response['warnings'], true));
             }
         }
 
