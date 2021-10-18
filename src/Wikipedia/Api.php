391978174305b87491761d198c9085f5ccc16f32
Damian Zaremba <damian@damianzaremba.co.uk>
2021-10-18 14:38:36 +0200

    Add $checkrun logic back
    This ensures we can disable the bot through Wikipedia at runtime

--- src/Wikipedia/Api.php
+++ src/Wikipedia/Api.php
@@ -468,6 +468,17 @@ class Api
         $wpq = new WikipediaQuery();
         $wpq->queryurl = str_replace('api.php', 'query.php', $this->apiurl);
 
+        if ($checkrun == true) {
+            if (
+                !preg_match(
+                    '/(yes|enable|true)/iS',
+                    ((isset($run)) ? $run : $wpq->getpage('User:' . $user . '/Run'))
+                )
+            ) {
+                return false;
+            }
+        } /* Check /Run page */
+
         $params = array(
             'action' => 'edit',
             'format' => 'php',
